6667b67b9c59af180961564fb1a36c7d15df43d4
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-10 18:55:32 +0300

    preparing the necessary environment

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const path = require('path')
+const PORT  = process.env.PORT
+const connectDB = require('./src/utilities/database')
+const ejs = require('ejs')
+const expressLayouts = require('express-ejs-layouts')
+
+
+connectDB()
+
+app.use(express.static('public'))
+
+
+app.set("view engine","ejs")
+app.set("views", path.resolve(__dirname,"./src"))
+app.use(expressLayouts)
+
+
+
+app.get('/',(req,res) => {
+
+   res.json({hi:'Hello'})
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Connected : http://localhost:${PORT}`)
+})
---
+++ src/utilities/database.js
@@ -0,0 +1,23 @@
+const mongoose  = require('mongoose')
+require('dotenv').config()
+
+const connectDB = async () => {
+
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser : true,
+            useUnifiedTopology : true,
+            useFindAndModify : false,
+            useCreateIndex : false
+        })
+
+        console.log("MongoDB connection successfully...")
+    }
+    catch(err) {
+        console.log(err)
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectDB
