2c1606216e926430023720c1b603e40424cfb675
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-12 03:09:57 +0300

    edited & added navbar

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,13 +4,6 @@
 
 
 exports.getIndex = (req,res) => {
-    let sayac=0
-    if(req.session.sayac) {
-        req.session.sayac++;
-    }else {
-        req.session.sayac = 1;
-    }
-
-
-    res.render('index', { sayac : req.session.sayac})
+  
+    res.render('index')
 }
